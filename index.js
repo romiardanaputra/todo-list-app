f4d62f7c0ccaf1c06e0797e4071c716d21a5f35f
Kadek Romi Ardana Putra <romiardana21@gmail.com>
2023-12-27 13:12:54 +0800

    finishing task-check function, adjusting styling css

--- index.js
+++ index.js
@@ -7,7 +7,20 @@ const taskListContainer = document.querySelector(".task-list-container");
 let counter = 0;
 
 const displayTaskCounter = (counter) => {
+   counter = Math.max(0, counter);
    taskCount.innerText = counter;
+   console.log(counter);
+};
+
+const createTaskListElement = (taskName) => {
+   return `
+   <div class="task-list">
+         <input type="checkbox" onchange="taskCheck(this)" class="task-check"/>
+         <div class="task-name">${taskName}</div>
+         <button class="edit-button" onclick="editTask(this)">Edit</button>
+         <button class="delete-button" onclick="deleteTask(this)">Delete</button>
+   </div>
+   `;
 };
 
 const addTask = () => {
@@ -22,34 +35,38 @@ const addTask = () => {
    const taskList = createTaskListElement(taskName);
    taskListContainer.insertAdjacentHTML("beforeend", taskList);
    taskInputField.value = "";
-};
-
-const createTaskListElement = (taskName) => {
-   return `
-   <div class="task-list">
-       <input type="checkbox" class="task-check"/>
-       <span class="task-name">${taskName}</span>
-       <div class="btn-wrappers">
-         <button class="edit-button" onclick="editTask(this)">Edit</button>
-         <button class="delete-button" onclick="deleteTask(this)">Delete</button>
-       </div>
-   </div>
-   `;
+   counter++;
+   displayTaskCounter(counter);
 };
 
 const deleteTask = (deleteButton) => {
    const taskList = deleteButton.closest(".task-list");
+   const taskInputCheckBox = taskList.querySelector(".task-check");
    taskList.remove();
-   counter--;
+   if (!taskInputCheckBox.checked) {
+      counter--;
+   }
    displayTaskCounter(counter);
 };
 
 const editTask = (editButton) => {
    const taskList = editButton.closest(".task-list");
    const taskName = taskList.querySelector(".task-name");
+   const taskInputCheckBox = taskList.querySelector(".task-check");
    taskInputField.value = taskName?.innerText;
    taskList.remove();
-   console.log(taskInputField.value);
+   if (!taskInputCheckBox.checked) {
+      counter--;
+   }
+   displayTaskCounter(counter);
+};
+
+const taskCheck = (inputChecked) => {
+   const taskList = inputChecked.closest(".task-list");
+   const taskInputCheckBox = taskList.querySelector(".task-check");
+   taskInputCheckBox.classList.toggle("completed");
+   taskInputCheckBox.checked ? counter-- : counter++;
+   displayTaskCounter(counter);
 };
 
 addButton.addEventListener("click", addTask);
